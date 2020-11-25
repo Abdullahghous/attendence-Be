9693a4d963051293d5c83559fa7390dddee4d78c
Muhammad Abdullah Ghous <30926876+Abdullahghous@users.noreply.github.com>
2020-11-25 15:16:29 +0500

    change in connection file

--- config/connection.js
+++ config/connection.js
@@ -14,21 +14,21 @@ const testing = {
   dialect:'postgres',
 };
 
-// const production = {
-//   database: 'attendence-app',
-//   username: 'postgres',
-//   password: '12345',
-//   host: '127.0.0.1:5432',
-//   dialect:'postgres',
-// };
-
 const production = {
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  host: process.env.DB_HOST || 'localhost',
-  dialect: 'postgres',
+  database: 'd336ee17j2cf93',
+  username: 'ljduhgjvorznwk',
+  password: '56d9ad57618523b3e1b93f6ef9906fb56129ed216dc273a0b820370f663304f1',
+  host: 'ec2-54-159-138-67.compute-1.amazonaws.com',
+  dialect:'postgres',
 };
+
+// const production = {
+//   database: process.env.DB_NAME,
+//   username: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   host: process.env.DB_HOST || 'localhost',
+//   dialect: 'postgres',
+// };
 
 module.exports = {
   development,
