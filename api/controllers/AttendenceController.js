56895387b2c6f1f293f0c189da80feb8d6a32574
Muhammad Abdullah Ghous <30926876+Abdullahghous@users.noreply.github.com>
2020-11-25 18:52:15 +0500

    mark attendence api change

--- api/controllers/AttendenceController.js
+++ api/controllers/AttendenceController.js
@@ -1,23 +1,24 @@
-const Attendence = require('../models/Attendence');
+const Attendence = require("../models/Attendence");
 
 const AttendenceController = () => {
   const markAttendence = async (req, res) => {
     try {
       let { mark, fine } = req.body;
       // validate
-      if (!mark || !fine)
+      if (!mark || !fine) {
         return res.status(400).json({ msg: "field cannot be empty" });
-          const newAttendence = ({
-            mark,
-            fine,
-            UserId : req.me.id
-          });
-          const data = await Attendence.create(newAttendence);
-          return res.status(201).json(data);
+      }
+      const newAttendence = {
+        mark,
+        fine,
+        UserId: req.me.id,
+      };
+      const data = await Attendence.create(newAttendence);
+      return res.status(201).json(data);
     } catch (err) {
       res.status(500).json({ error: err.message });
     }
-};
+  };
 
   return {
     markAttendence,
