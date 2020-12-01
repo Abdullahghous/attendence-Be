8876086948d94217c28ac1b192724727a5dadba3
Muhammad Abdullah Ghous <30926876+Abdullahghous@users.noreply.github.com>
2020-12-01 15:13:06 +0500

    change in token expire time

--- api/services/auth.service.js
+++ api/services/auth.service.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const secret = require('../utils').secret;
 
 const authService = () => {
-  const issue = (payload) => jwt.sign(payload, secret(), { expiresIn: 10800 });
+  const issue = (payload) => jwt.sign(payload, secret());
   const verify = (token, cb) => jwt.verify(token, secret(), {}, cb);
 
   return {
