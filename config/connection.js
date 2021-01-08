5a56e503de6666f986f63cad0f986090ed60908d
Muhammad Abdullah Ghous <30926876+Abdullahghous@users.noreply.github.com>
2021-01-08 16:51:28 +0500

    changes

--- config/connection.js
+++ config/connection.js
@@ -18,7 +18,7 @@ const production = {
   database: 'demcurrida6b6j',
   username: 'phwhffkjabwblg',
   password: '9414e22e6d03db6fb6dfdb29b417011a813abfa15f2a7f7a68cf8f3d2ce47fcd',
-  host: 'ec2-54-196-133-177.compute-1.amazonaws.comm',
+  host: 'ec2-54-196-133-177.compute-1.amazonaws.com',
   dialect: 'postgres',
 };
 
